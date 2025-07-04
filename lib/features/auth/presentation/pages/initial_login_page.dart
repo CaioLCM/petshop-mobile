8f8887105ff3e9e4cd5675f540364f7f5b5c239a
Caio Lene Campos Magalhães <caio.lene.magalhaes@gmail.com>
2025-07-04 00:37:00 -0300

    Criei a pagina inicial login

--- lib/features/auth/presentation/pages/initial_login_page.dart
+++ lib/features/auth/presentation/pages/initial_login_page.dart
@@ -11,9 +11,83 @@ class _InitialLoginPageState extends State<InitialLoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
+      body: Stack(
         children: [
-          Text("Initial page")
+          Container(
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                colors: [Color(0xFFF0F9FF), Color(0xFF909599)],
+              ),
+            ),
+          ),
+          Center(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: EdgeInsets.fromLTRB(0, 231, 0, 0),
+                  child: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Image(
+                        image: AssetImage("assets/images/cat.png"),
+                        width: 70,
+                        height: 80,
+                      ),
+                      SizedBox(width: 0),
+                      Text(
+                        "PetGateu",
+                        style: TextStyle(
+                          fontFamily: "Nunito",
+                          fontWeight: FontWeight.w900,
+                          letterSpacing: 0,
+                          fontSize: 48,
+                          color: Color(0xFF020A22),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(25, 140, 0, 0),
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      minimumSize: Size(343, 44),
+                      backgroundColor: Color(0xFF3B82F6),
+                      foregroundColor: Color(0xFF020A22),
+                      side: BorderSide(width: 2, color: Color(0xFF60A5FA)),
+                      // To customize border, use side: BorderSide(...)
+                    ),
+                    onPressed: () {},
+                    child: Text(
+                      "Fazer login",
+                      style: TextStyle(
+                        fontFamily: "Nunito",
+                        fontWeight: FontWeight.w500,
+                        fontSize: 16,
+                        letterSpacing: 0,
+                      ),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.fromLTRB(25, 10, 0, 0),
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      minimumSize: Size(343, 44),
+                      backgroundColor: Color(0xFFFCFCFC),
+                      foregroundColor: Color(0xFF020A22),
+                      side: BorderSide(width: 2, color: Color(0xFF60A5FA)),
+                    ),
+                    child: Text("Cadastro", style: TextStyle(fontFamily: "Nunito", fontWeight: FontWeight.w500, fontSize: 16, letterSpacing: 0),),
+                  ),
+                ),
+              ],
+            ),
+          ),
         ],
       ),
     );
