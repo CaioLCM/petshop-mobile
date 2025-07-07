036dd08d85d1b71dfa762cbda8545fd423c7ec39
Caio Lene Campos Magalhães <caio.lene.magalhaes@gmail.com>
2025-07-07 11:40:32 -0300

    home page + perfil + seção de navegação

---
+++ lib/features/estoque/presentation/pages/estoque_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class EstoquePage extends StatelessWidget {
+  const EstoquePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
